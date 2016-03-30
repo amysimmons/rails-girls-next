fac0584c0a0d40965ef5f9144acbceb829efed94
Amy Simmons <amy__simmons@hotmail.com>
2016-03-30 20:45:46 +1100

    roman method refactored

--- tdd/roman.rb
+++ tdd/roman.rb
@@ -1,7 +1,21 @@
 require "pry"
 
 def roman(n)
-  return "I";
+  numbers = {
+    1 => "I",
+    4 => "IV",
+    5 =>"V",
+    6 =>"VI",
+    7 =>"VII",
+    9 =>"IX",
+    10 => "X",
+    50 => "L",
+    100=> "C",
+    500 =>"D",
+    1000 => "M"
+  }
+
+  return numbers[n];
 end
 
 require "minitest/spec"
