6c42e7eed2f2863eb4ef191a97b96dc839577d4d
Amy Simmons <amy__simmons@hotmail.com>
2016-03-31 19:58:28 +1100

    atm returning number of notes, not just true

--- atm/atm.rb
+++ atm/atm.rb
@@ -2,10 +2,9 @@ def withdraw(amount)
   if amount <= 0 # this deals with some of the situations...
     return false
   end
-  # ToDo: figure out this bit
 
   if amount % 5 == 0
-    return true
+    return amount / 5
   else
     return false
   end
@@ -18,16 +17,17 @@ require 'minitest/autorun'
 # BELOW ARE THE TESTS FOR AUTOMATICALLY CHECKING YOUR SOLUTION.
 # THESE TESTS ARE FOR STEP 1.
 # THESE NEED TO BE REPLACED AFTER EACH STEP.
+# Replace your existing tests with the ones below.
 describe 'atm' do
   [
     [-1, false],
     [0, false],
     [1, false],
     [43, false],
-    [17, false],
-    [5, true],
-    [20, true],
-    [35, true],
+    [7, false],
+    [5, 1],
+    [20, 4],
+    [35, 7],
   ].each do |input, expected|
     it "should return #{expected} when $#{input} is withdrawn" do
       withdraw(input).must_equal expected
