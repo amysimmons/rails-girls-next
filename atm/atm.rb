db7bda1ba29f8a7781ff7694e7edbbc056cfc7d2
Amy Simmons <amy__simmons@hotmail.com>
2016-03-31 20:05:18 +1100

    atm returns array of 5 dollar notes

--- atm/atm.rb
+++ atm/atm.rb
@@ -1,10 +1,13 @@
 def withdraw(amount)
+  notes = []
+
   if amount <= 0 # this deals with some of the situations...
     return false
   end
 
   if amount % 5 == 0
-    return amount / 5
+    (amount / 5).times do notes << 5 end
+    return notes
   else
     return false
   end
@@ -18,16 +21,15 @@ require 'minitest/autorun'
 # THESE TESTS ARE FOR STEP 1.
 # THESE NEED TO BE REPLACED AFTER EACH STEP.
 # Replace your existing tests with the ones below.
+# Replace your existing tests with the ones below.
 describe 'atm' do
   [
     [-1, false],
     [0, false],
     [1, false],
     [43, false],
-    [7, false],
-    [5, 1],
-    [20, 4],
-    [35, 7],
+    [20, [5, 5, 5, 5]],
+    [35, [5, 5, 5, 5, 5, 5, 5]],
   ].each do |input, expected|
     it "should return #{expected} when $#{input} is withdrawn" do
       withdraw(input).must_equal expected
